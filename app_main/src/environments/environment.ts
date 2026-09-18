bd907f8ba785b9277a1f74985d0ab9d7cb3ca051
vgrs2292 <167320276+victorsismotto2292@users.noreply.github.com>
2026-09-18 11:56:24 -0300

    Colocando configs da firebase
    Alterando definições do environment.ts

--- app_main/src/environments/environment.ts
+++ app_main/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNpwqZiDawALsXVK3AOBTITUXmzvwzs0I",
+    authDomain: "ionicfirebase-73af7.firebaseapp.com",
+    projectId: "ionicfirebase-73af7",
+    storageBucket: "ionicfirebase-73af7.firebasestorage.app",
+    messagingSenderId: "1063925869269",
+    appId: "1:1063925869269:web:6b12b270c811b1564f905c",
+    measurementId: "G-PLN0V1QC93"
+  }
 };
 
 /*
