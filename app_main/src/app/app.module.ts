a614f01343d212bc853112c7b5dc809c0f73fd1e
vgrs2292 <167320276+victorsismotto2292@users.noreply.github.com>
2026-09-18 12:00:40 -0300

    Configurações da app.module.ts
    Implementando configurações

--- app_main/src/app/app.module.ts
+++ app_main/src/app/app.module.ts
@@ -7,11 +7,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular/lazy';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Importações do SDK Modular do Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'; // Inicializar o firebase
+import { getFirestore, provideFirestore } from '@angular/fire/firestore' // Funções para o Firestore
+
+import { environment } from '../environments/environment'; // Configurações do ambiente
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // Provisão do firestore
+    provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent],
 })
