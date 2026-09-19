329499aae57abd35745cc8cd9ba5625176cbd81e
= <167320276+victorsismotto2292@users.noreply.github.com>
2026-09-19 18:08:13 -0300

    Código home.page.ts
    Código completo Typescript home page

--- app_main/src/app/home/home.page.ts
+++ app_main/src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+// NOVAS EXTENSÕES:
+import { DataService, Item } from '../services/data.service';
+import { Data, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular/lazy';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,7 +12,50 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  items: Item[] = [];
 
-  constructor() {}
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit(){
+    this.dataService.getItems().subscribe(res => {
+      this.items = res;
+    })
+  }
+
+  // Adicionar item:
+  addItem(){
+    this.router.navigateByUrl('/item-detail');
+  }
+
+  // Editar item:
+  editItem(item: Item){
+    this.router.navigateByUrl(`/item-detail/${item.id}`);
+  }
+
+  // Excluir item (forma assíncrona):
+  async deleteItem(id: string){
+    const alert = await this.alertController.create({
+      header: 'Confirmar exclusão',
+      message: 'Tem certeza que deseja excluir esse item?',
+      buttons:[
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Excluir',
+          handler: () => {
+            this.dataService.deleteItem(id);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 
 }
